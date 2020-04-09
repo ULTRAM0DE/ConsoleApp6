818e8614fe30955fa6c020757cd9356e6d6a7416
ULTRAM0DE <63294340+ULTRAM0DE@users.noreply.github.com>
2020-04-09 11:40:34 +0400

    Add files via upload

---
+++ Программирование 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<short> digits = new List<short>() { 1, 0, 93, 75 };
+            digits.Add(5);
+            digits.Add(15);
+            digits.Add(8);
+            digits.Add(45);
+            Console.WriteLine(digits[4]);
+
+            Console.ReadKey()
+                ;
+
+        }
+    }
+}
